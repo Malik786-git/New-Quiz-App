7b67ef327294e03e7d530c8d462963708797fcfe
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-01-27 02:33:06 +0500

    update more

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const start = document.getElementById('startbtn');
 const container = document.getElementById('container');
-start.onclick = ()=>{
-    container.innerHTML = "";
+const next = document.getElementById('next');
+
+start.onclick = () => {
+    container.innerHTML = `
+    <div class="questionContainer">
+
+    <h3>what the name of pakistan?</h3>
+    <input type="radio" name="same">  Heel
+    <br>
+    <input type="radio" name="same">  Hello
+    <br>
+    <input type="radio" name="same">  paksitan
+    <br>
+    <input type="radio" name="same">  pakistan
+    <br>
+    <br>
+    <button id="next">Next</button>
+    </div>
+    `;
+    
 }
