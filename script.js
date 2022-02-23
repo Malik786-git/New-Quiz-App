a64d19d92ea2e6e10b355a6a0cdb0b9b89951c74
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-02-24 01:13:14 +0500

    adfasd

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 const start = document.getElementById('startbtn');
 const container = document.getElementById('container');
 const next = document.getElementById('next');
+const subject = document.getElementById('subject');
 
-start.onclick = () => {
+const updateQuiz = (data) => {
+   console.log(data[0].options[0]);
     container.innerHTML = `
     <div class="questionContainer">
 
-    <h3>what the name of pakistan?</h3>
-    <input type="radio" name="same">  Heel
+    <h3>${data[0].question}</h3>
+    <input type="radio" name="same">  ${data[0].options[0]}
     <br>
-    <input type="radio" name="same">  Hello
+    <input type="radio" name="same">  ${data[0].options[1]}
     <br>
-    <input type="radio" name="same">  paksitan
+    <input type="radio" name="same">  ${data[0].options[2]}
     <br>
-    <input type="radio" name="same">  pakistan
+    <input type="radio" name="same">  ${data[0].options[3]}
     <br>
     <br>
     <button id="next">Next</button>
     </div>
-    `;
+     `;
+}
+
+
+const startQuiz = () => {
+
+    if (subject.value === 'cs') {
+
+        fetch('question.json')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data.cs);
+                updateQuiz(data.cs)
+        
+            });
+
+    } else {
+
     
+    }
+
+}
+
+
+
+start.onclick = () => {
+    startQuiz();
 }
